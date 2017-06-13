d302158d800d28bf8d1fd76147ce955cbfcb6b85
Christian Schneider <chris@die-schneider.net>
2017-06-13 11:48:05 +0200

    Use current API

--- src/main/java/net/lr/jollydays/HolidaysTest.java
+++ src/main/java/net/lr/jollydays/HolidaysTest.java
@@ -6,15 +6,15 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 import de.jollyday.Holiday;
-import de.jollyday.HolidayCalendar;
 import de.jollyday.HolidayManager;
+import de.jollyday.parameter.CalendarPartManagerParameter;
 
 @Component(immediate=true)
 public class HolidaysTest {
 
     @Activate
     public void start()  {
-        HolidayManager manager = HolidayManager.getInstance(HolidayCalendar.GERMANY);
+        HolidayManager manager = HolidayManager.getInstance(new CalendarPartManagerParameter("de", null));
         Set<Holiday> holidays = manager.getHolidays(2017);
         for (Holiday holiday : holidays) {
             System.out.println(holiday.getDate());
